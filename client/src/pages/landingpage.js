7fd9f668bc813c37ce394b4baffe9c722b020171
Napassorn B <nanaps.napassorn@gmail.com>
2022-11-08 00:35:41 +0700

    add:landingpage

--- client/src/pages/landingpage.js
+++ client/src/pages/landingpage.js
@@ -1,16 +1,33 @@
-import { Box, Image } from "@chakra-ui/react";
+import { Box, Flex, Image } from "@chakra-ui/react";
 
 export function Landingpage() {
   return (
-    <Box w="100vw" h="100vh" bg="white">
-      <Image src="picture/banner.png" alt="watch banner" />
-      <Box w="100vw" h="30px"></Box>
-      <Box w="736px" h="265px">
-        DISCOVER YOUR BEST WATCH 
-        From the biggest designer brands, to the best
-        of luxury and hidden gems. See our featured brands.
+    <Box>
+      <Box w="100vw" h="100vh" bg="white">
+        <Image src="picture/header.png" alt="white watch" />
+        <Box m="30px">
+          <Image src="picture/banner.png" alt="watch banner" />
+        </Box>
+        <Box mt="30px" mb="30px">
+          <Flex>
+            <Box w="736px" h="265px" ml="55px" mr="15px" p="40px">
+              <h1>DISCOVER YOUR BEST WATCH</h1>
+              <p>
+                From the biggest designer brands, to the best of luxury and
+                hidden gems. See our featured brands.
+              </p>
+            </Box>
+            <Image
+              src="picture/discover-watch.png"
+              alt="Discover your best watch"
+            />
+          </Flex>
+        </Box>
+        <Image src="picture/footer-watch.png" alt="Black watch" />
+        <Box w="100vw" h="80px" bg=" #D6BE96" center>
+          Copyright©2022 Watchodile.All Right Reserved.
+        </Box>
       </Box>
-      <Image src="picture/discover-watch.png" alt="Discover your best watch" />
     </Box>
   );
 }
