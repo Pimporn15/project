701af4a890a2cf1ffff00e196164174b15ba7373
Napassorn B <nanaps.napassorn@gmail.com>
2022-11-08 11:36:14 +0700

    update:landingpage

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,12 @@ import React from "react";
 // 1. import `ChakraProvider` component
 import { ChakraProvider } from "@chakra-ui/react";
 import { Landingpage } from "./pages/landingpage";
-import { Navbar } from "./components/navbar";
+
 
 function App() {
   // 2. Wrap ChakraProvider at the root of your app
   return (
     <ChakraProvider>
-      <Navbar />
       <Landingpage />
     </ChakraProvider>
   );
--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -3,70 +3,60 @@ import {
   Breadcrumb,
   BreadcrumbItem,
   BreadcrumbLink,
-  BreadcrumbSeparator,
-  Heading,
   Image,
   Text,
-  Flex,
-  Spacer,
+  Center,
 } from "@chakra-ui/react";
-import {
-  Menu,
-  MenuButton,
-  MenuList,
-  MenuItem,
-  MenuItemOption,
-  MenuGroup,
-  MenuOptionGroup,
-  MenuDivider,
-  Portal,
-} from "@chakra-ui/react";
+import { Menu, MenuButton, MenuList, MenuItem, Portal } from "@chakra-ui/react";
 
 export function Navbar() {
   return (
-    <Flex position="relative" gap="2">
-      <Box bg="#D6BE96" w="100%" p="5">
-        <Breadcrumb justifyContent="center">
-          <BreadcrumbItem>
-            <Text href="#" fontSize="24px" color="#000000">
-              HOME
-            </Text>
-          </BreadcrumbItem>
-          <BreadcrumbItem>
-            <Menu>
-              <MenuButton fontSize="24px" color="#000000">
-                PRODUCTS
-              </MenuButton>
-              <Portal>
-                <MenuList>
-                  <MenuItem>New PRODUCTS</MenuItem>
-                  <MenuItem>Men</MenuItem>
-                  <MenuItem>Women</MenuItem>
-                  <MenuItem>KID</MenuItem>
-                  <MenuItem>ACCESSORIES</MenuItem>
-                </MenuList>
-              </Portal>
-            </Menu>
-          </BreadcrumbItem>
-          <BreadcrumbItem>
-            <Text href="#" fontSize="24px" color="#000000">
-              REGISTER
-            </Text>
-          </BreadcrumbItem>
+    <Center bg="#C7BAB1" justifyContent="center" >
+      {/* <Flex position="relative" gap="2" bg="#C7BAB1" > */}
+        <Box bg="#D6BE96" w="900px" p="4"  >
+      
+          <Breadcrumb justifyContent="center" alignSelf='flex-end'>
+            <BreadcrumbItem>
+              <Text href="#" fontSize="24px" color="#000000">
+                HOME
+              </Text>
+            </BreadcrumbItem>
+            <BreadcrumbItem>
+              <Menu>
+                <MenuButton fontSize="24px" color="#000000">
+                  PRODUCTS
+                </MenuButton>
+                <Portal>
+                  <MenuList>
+                    <MenuItem>New PRODUCTS</MenuItem>
+                    <MenuItem>Men</MenuItem>
+                    <MenuItem>Women</MenuItem>
+                    <MenuItem>KID</MenuItem>
+                    <MenuItem>ACCESSORIES</MenuItem>
+                  </MenuList>
+                </Portal>
+              </Menu>
+            </BreadcrumbItem>
+            <BreadcrumbItem>
+              <Text href="#" fontSize="24px" color="#000000">
+                REGISTER
+              </Text>
+            </BreadcrumbItem>
 
-          <BreadcrumbItem>
-            <Text href="#" fontSize="24px" color="#000000">
-              LOGIN
-            </Text>
-          </BreadcrumbItem>
+            <BreadcrumbItem>
+              <Text href="#" fontSize="24px" color="#000000">
+                LOGIN
+              </Text>
+            </BreadcrumbItem>
 
-          <BreadcrumbItem>
-            <BreadcrumbLink href="#">
-              <Image src="/picture/cart.png" alt="cart" />
-            </BreadcrumbLink>
-          </BreadcrumbItem>
-        </Breadcrumb>
-      </Box>
-    </Flex>
+            <BreadcrumbItem>
+              <BreadcrumbLink href="#">
+                <Image src="/picture/cart.png" alt="cart" />
+              </BreadcrumbLink>
+            </BreadcrumbItem>
+          </Breadcrumb>
+        </Box>
+      {/* </Flex> */}
+    </Center>
   );
 }
