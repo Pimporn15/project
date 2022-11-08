7c38e1cdd89fa8c4d6faa002d8f9c3e7076db4ea
Napassorn B <nanaps.napassorn@gmail.com>
2022-11-08 11:36:14 +0700

    update:landingpage

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { ChakraProvider } from "@chakra-ui/react";
 import { Landingpage } from "./pages/landingpage";
-import { Navbar } from "./components/navbar";
 
 function App() {
   return (
--- client/src/pages/landingpage.js
+++ client/src/pages/landingpage.js
@@ -1,33 +1,41 @@
-import { Box, Flex, Image } from "@chakra-ui/react";
+import { Box, Flex, Image, Center } from "@chakra-ui/react";
+import { Navbar } from "../components/navbar";
 
 export function Landingpage() {
   return (
     <Box>
-      <Box w="100vw" h="100vh" bg="white">
-        <Image src="picture/header.png" alt="white watch" />
-        <Box m="30px">
-          <Image src="picture/banner.png" alt="watch banner" />
-        </Box>
-        <Box mt="30px" mb="30px">
-          <Flex>
-            <Box w="736px" h="265px" ml="55px" mr="15px" p="40px">
-              <h1>DISCOVER YOUR BEST WATCH</h1>
-              <p>
-                From the biggest designer brands, to the best of luxury and
-                hidden gems. See our featured brands.
-              </p>
+      <Center w="100vw" bg="#C7BAB1">
+        <Flex>
+          <Box w="900px">
+            <Navbar />
+            <Image src="picture/header.png" alt="white watch" />
+            <Box mt="20px" mb="20px">
+              <Image src="picture/banner.png" alt="watch banner" />
             </Box>
-            <Image
-              src="picture/discover-watch.png"
-              alt="Discover your best watch"
-            />
-          </Flex>
-        </Box>
-        <Image src="picture/footer-watch.png" alt="Black watch" />
-        <Box w="100vw" h="80px" bg=" #D6BE96" center>
-          Copyright©2022 Watchodile.All Right Reserved.
-        </Box>
-      </Box>
+            <Box mt="20px" mb="20px">
+              <Flex>
+                <Box w="400px" h="350px" p="15px" bg="#7E5B31">
+                  <h1 fontSize="40px" color="#ffffff">
+                    DISCOVER YOUR BEST WATCH
+                  </h1>
+                  <p fontSize="32px" color="#ffffff">
+                    From the biggest designer brands, to the best of luxury and
+                    hidden gems. See our featured brands.
+                  </p>
+                </Box>
+                <Image
+                  src="picture/discover-watch.png"
+                  alt="Discover your best watch"
+                />
+              </Flex>
+            </Box>
+            <Image src="picture/footer-watch.png" alt="Black watch" />
+            <Center w="900px" h="60px" bg=" #D6BE96">
+              Copyright©2022 Watchodile.All Right Reserved.
+            </Center>
+          </Box>
+        </Flex>
+      </Center>
     </Box>
   );
 }
