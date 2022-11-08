54d3ea36bcf622989a52b7a11bd56ba111b7a0a5
Napassorn B <nanaps.napassorn@gmail.com>
2022-11-08 21:54:31 +0700

    feat:loginpage

--- client/src/App.js
+++ client/src/App.js
@@ -1,45 +1,13 @@
 import React from "react";
 import { ChakraProvider } from "@chakra-ui/react";
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-import {Register} from "./pages/Registerpage";
-=======
-import { Register } from "./pages/Registerpage";
->>>>>>> 8e92e5a (updatestate)
-=======
 import Register from "./pages/Registerpage";
->>>>>>> 7bd36fd (rebase)
-=======
 import { Register } from "./pages/Registerpage";
->>>>>>> c743d2c (git merge 11-09-65)
-=======
-// import { Landingpage } from "./pages/landingpage";
-import { LoginPage } from "./pages/loginPage";
 
-import Register from "./pages/Registerpage";
->>>>>>> c523b63 ( edit config after merge)
-
-//สร้าง route
 function App() {
   return (
     <ChakraProvider>
-<<<<<<< HEAD
-<<<<<<< HEAD
-      <ProductListingPage />
-      {/* <CartsPage/> */}
-      {/* <Landingpage /> */}
-      <Navbar />
-      <Landingpage /> <Register />
-=======
-=======
-      <LoginPage />
-
->>>>>>> c523b63 ( edit config after merge)
       <Register />
->>>>>>> 8e92e5a (updatestate)
     </ChakraProvider>
   );
 }
