281ba7a1ed0592d6c4109a132b2c42f7e7f9ddd4
Napassorn B <nanaps.napassorn@gmail.com>
2022-11-08 21:54:31 +0700

    feat:loginpage

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,15 @@
 import React from "react";
 
-// 1. import `ChakraProvider` component
 import { ChakraProvider } from "@chakra-ui/react";
-import { Landingpage } from "./pages/landingpage";
+// import { Landingpage } from "./pages/landingpage";
+import { LoginPage } from "./pages/loginPage";
+
 
 function App() {
   // 2. Wrap ChakraProvider at the root of your app
   return (
     <ChakraProvider>
-      <Landingpage />
+      <LoginPage />
     </ChakraProvider>
   );
 }
