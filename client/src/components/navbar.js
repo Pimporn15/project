ba3a30b872bcf284a2edad47f4a453c5517eb866
Napassorn B <nanaps.napassorn@gmail.com>
2022-11-09 11:14:46 +0700

    update:runtime-coponent

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -4,58 +4,66 @@ import {
   BreadcrumbItem,
   BreadcrumbLink,
   Image,
-  Text,
+  // Text,
   Center,
+  Button,
 } from "@chakra-ui/react";
 import { Menu, MenuButton, MenuList, MenuItem, Portal } from "@chakra-ui/react";
 
 export function Navbar() {
   return (
-    <Center bg="#C7BAB1" justifyContent="center" >
+    <Center bg="#C7BAB1" justifyContent="center">
       {/* <Flex position="relative" gap="2" bg="#C7BAB1" > */}
-        <Box bg="#D6BE96" w="900px" p="4"  >
-      
-          <Breadcrumb justifyContent="center" alignSelf='flex-end'>
-            <BreadcrumbItem>
-              <Text href="#" fontSize="24px" color="#000000">
-                HOME
-              </Text>
-            </BreadcrumbItem>
-            <BreadcrumbItem>
-              <Menu>
-                <MenuButton fontSize="24px" color="#000000">
-                  PRODUCTS
-                </MenuButton>
-                <Portal>
-                  <MenuList>
-                    <MenuItem>New PRODUCTS</MenuItem>
-                    <MenuItem>Men</MenuItem>
-                    <MenuItem>Women</MenuItem>
-                    <MenuItem>KID</MenuItem>
-                    <MenuItem>ACCESSORIES</MenuItem>
-                  </MenuList>
-                </Portal>
-              </Menu>
-            </BreadcrumbItem>
-            <BreadcrumbItem>
-              <Text href="#" fontSize="24px" color="#000000">
-                REGISTER
-              </Text>
-            </BreadcrumbItem>
+      <Box bg="#D6BE96" w="900px" p="4">
+        <Breadcrumb justifyContent="center" alignSelf="flex-end">
+          <BreadcrumbItem>
+            <Button bg="#D6BE96" fontSize="24px" color="#000000">
+              HOME
+            </Button>
+          </BreadcrumbItem>
+          <BreadcrumbItem>
+            <Menu>
+              <MenuButton fontSize="24px" color="#000000">
+                PRODUCTS
+              </MenuButton>
+              <Portal>
+                <MenuList>
+                  <MenuItem>New PRODUCTS</MenuItem>
+                  <MenuItem>Men</MenuItem>
+                  <MenuItem>Women</MenuItem>
+                  <MenuItem>KID</MenuItem>
+                  <MenuItem>ACCESSORIES</MenuItem>
+                </MenuList>
+              </Portal>
+            </Menu>
+          </BreadcrumbItem>
+          <BreadcrumbItem>
+            <Button bg="#D6BE96" href="#" fontSize="24px" color="#000000">
+              REGISTER
+            </Button>
+          </BreadcrumbItem>
 
-            <BreadcrumbItem>
-              <Text href="#" fontSize="24px" color="#000000">
-                LOGIN
-              </Text>
-            </BreadcrumbItem>
+          <BreadcrumbItem>
+            <Button bg="#D6BE96" fontSize="24px" color="#000000">
+              LOGIN
+            </Button>
+          </BreadcrumbItem>
 
-            <BreadcrumbItem>
-              <BreadcrumbLink href="#">
+          <BreadcrumbItem>
+            <Button bg="#D6BE96" fontSize="24px" color="#000000">
+              LOGOUT
+            </Button>
+          </BreadcrumbItem>
+
+          <BreadcrumbItem>
+            <BreadcrumbLink href="#">
+              <Button bg="#D6BE96">
                 <Image src="/picture/cart.png" alt="cart" />
-              </BreadcrumbLink>
-            </BreadcrumbItem>
-          </Breadcrumb>
-        </Box>
+              </Button>
+            </BreadcrumbLink>
+          </BreadcrumbItem>
+        </Breadcrumb>
+      </Box>
       {/* </Flex> */}
     </Center>
   );
