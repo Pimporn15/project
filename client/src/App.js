8e92e5aca00afdcc2387ac3e5beaf78d39ad5724
Napassorn B <nanaps.napassorn@gmail.com>
2022-11-09 19:52:17 +0700

    updatestate

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,12 @@ import React from "react";
 
 import { ChakraProvider } from "@chakra-ui/react";
 
-import {Register} from "./pages/Registerpage";
-
+import { Register } from "./pages/Registerpage";
 
 function App() {
- 
   return (
     <ChakraProvider>
-      <Register/>
-     
+      <Register />
     </ChakraProvider>
   );
 }
