3378ab4b57bb0b34e3c6ca5b29f57e26fdeecf69
Napassorn B <nanaps.napassorn@gmail.com>
2022-11-09 19:43:55 +0700

    create state

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { ChakraProvider } from "@chakra-ui/react";
 
-import Register from "./pages/Registerpage";
+import {Register} from "./pages/Registerpage";
 
 function App() {
   return (
--- client/src/pages/Registerpage.js
+++ client/src/pages/Registerpage.js
@@ -1,127 +1,220 @@
-import { Flex, Box,FormControl,FormLabel,Input,InputGroup, InputRightElement, Stack, Button, Heading,
-  Text, useColorModeValue, Link,Image
-} from '@chakra-ui/react';
-import { useState } from 'react';
-import { ViewIcon, ViewOffIcon } from '@chakra-ui/icons';
-import React from 'react'
+import {
+  Flex,
+  Box,
+  FormControl,
+  Input,
+  InputGroup,
+  InputRightElement,
+  Stack,
+  Button,
+  Heading,
+  Text,
+  useColorModeValue,
+  Link,
+  Image,
+} from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+import { ViewIcon, ViewOffIcon } from "@chakra-ui/icons";
+import React from "react";
+import axios from "axios";
 
- 
+export function Register() {
+  // const [userProfile,setUserProfile] = useState({})
+  const [username, setUsername] = useState("");
+  const [firstName, setFirstname] = useState("");
+  const [lastName, setLastname] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [watch, setWatch] = useState([]);
 
-export default function Register() {
-  const [showPassword, setShowPassword] = useState(false);
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    console.log("yeah");
+  };
+  // const registerUser = async () => {
+  //   const response = await axios.post("http://localhost:4000/register");
+  // };
+
+  const registerUser = async () => {
+    const response = await axios.get("http://localhost:4000/");
+    setWatch(response.data.data);
+    console.log(response.data.data);
+  };
+
+  // const registerPost = async () => {
+  //   const response = await axios.post("http://localhost:4000/register",{
+  //     "username": "nannie",
+  //     "firstName":"napassorn",
+  //     "lastName": "Boo"
+  //   }),
+  //   setWatch(response.data.data);
+  //   console.log(response.data.data);
+  // };
+
+  useEffect(() => {
+    registerUser();
+  }, []);
 
   return (
+    <form
+      onSubmit={(event) => {
+        handleSubmit(event);
+      }}
+    >
+      <Box bg="white" w="100vw">
+        <Flex direction="row" gap="500px">
+          <Box>
+            <Box position="relative" bg="#2C2C2C" w="350px" h="100vh"></Box>
+            <Box position="absolute" top="100px">
+              <Image src="picture/imagereg.png" alt="watch picture" />
+            </Box>
+          </Box>
+          <Box>
+            <Flex align={"center"} justify={"center"}>
+              <Stack spacing={8} mx={"auto"} maxW={"lg"} py={100} px={6}>
+                <Stack align={"center"}>
+                  <Heading fontSize={"48px"} textAlign={"center"}>
+                    Register
+                  </Heading>
+                </Stack>
+                <Box
+                  w="500px"
+                  rounded={"lg"}
+                  // bg={useColorModeValue("white", "gray.700")}
+                  boxShadow={"lg"}
+                  p={8}
+                >
+                  <Stack spacing={4}>
+                    <Box>
+                      <FormControl id="UserName">
+                        {/* <FormLabel fontSize={'20px'} >UserName</FormLabel> */}
+                        <Input
+                          type="text"
+                          variant="flushed"
+                          placeholder="UserName"
+                          fontSize={"20px"}
+                          isInvalid
+                          errorBorderColor="#AA8B56"
+                          onChange={(event) => {
+                            setUsername(event.target.value);
+                          }}
+                          value={username}
+                        />
+                        {/* <hr color='black' /> */}
+                      </FormControl>
+                      <FormControl id="password" isRequired pt={4}>
+                        {/* <FormLabel  fontSize={'20px'} >Password</FormLabel> */}
+                        <InputGroup>
+                          <Input
+                            type={password ? "text" : "password"}
+                            variant="flushed"
+                            placeholder="password"
+                            fontSize={"20px"}
+                            isInvalid
+                            errorBorderColor="#AA8B56"
+                            onChange={(event) => {
+                              setPassword(event.target.value);
+                            }}
+                            value={password}
+                          />
+                          <InputRightElement h={"full"}>
+                            <Button
+                              variant={"ghost"}
+                              onClick={() =>
+                                setPassword((Password) => !Password)
+                              }
+                            >
+                              {password ? <ViewIcon /> : <ViewOffIcon />}
+                            </Button>
+                          </InputRightElement>
+                        </InputGroup>
+                      </FormControl>
+                    </Box>
 
-    <Box  bg='white' w='100vw' >
-      <Flex direction='row' gap='500px' >
-      <Box>
-      <Box position="relative" bg='#2C2C2C' w='350px' h='100vh'  ></Box>
-      <Box position='absolute' top='100px'>
-  <Image src='picture/imagereg.png' alt='watch picture'   />
-</Box>
-</Box>
-<Box>
-    <Flex
-      
-      align={'center'}
-      justify={'center'}
-     >
-      <Stack spacing={8} mx={'auto'} maxW={'lg'} py={100} px={6}>
-        <Stack align={'center'}>
-          <Heading fontSize={'48px'} textAlign={'center'}>
-          Register
-          </Heading>
-         
-        </Stack>
-        <Box
-          w='500px'
-          rounded={'lg'}
-          bg={useColorModeValue('white', 'gray.700')}
-          boxShadow={'lg'}
-          p={8}>
-          <Stack spacing={4}>
-         
-               
-              <Box>
-              
-                <FormControl id="UserName">
-                  {/* <FormLabel fontSize={'20px'} >UserName</FormLabel> */}
-                  <Input type="text" variant='flushed' placeholder='UserName'fontSize={'20px'}   isInvalid
-    errorBorderColor='#AA8B56'/>
-                  {/* <hr color='black' /> */}
-                  
-                </FormControl>
-                <FormControl id="password" isRequired  pt={4}>
-              {/* <FormLabel  fontSize={'20px'} >Password</FormLabel> */}
-              <InputGroup>
-                <Input type={showPassword ? 'text' : 'password'} variant='flushed' placeholder='password' fontSize={'20px'} isInvalid
-    errorBorderColor='#AA8B56' />
-                <InputRightElement h={'full'}>
-                  <Button
-                    variant={'ghost'} 
-                    onClick={() =>
-                      setShowPassword((showPassword) => !showPassword)
-                    }>
-                    {showPassword ? <ViewIcon /> : <ViewOffIcon />}
-                  </Button>
-                </InputRightElement>
-              </InputGroup>
-            </FormControl> 
-              </Box>
-              
-              <Box>
-                <FormControl id="firstName" isRequired>
-                  {/* <FormLabel fontSize={'20px'}>First Name</FormLabel> */}
-                  <Input type="text" variant='flushed' placeholder='FirstName' fontSize={'20px'}  isInvalid
-    errorBorderColor='#AA8B56'/>
-                </FormControl>
-              </Box>
-              <Box>
-                <FormControl id="lastName">
-                  {/* <FormLabel fontSize={'20px'} >Last Name</FormLabel> */}
-                  <Input type="text" variant='flushed' placeholder='Lastname' fontSize={'20px'} isInvalid
-    errorBorderColor='#AA8B56'/>
-                </FormControl>
-              </Box>
-             
-           
-           
-           
+                    <Box>
+                      <FormControl id="firstName" isRequired>
+                        {/* <FormLabel fontSize={'20px'}>First Name</FormLabel> */}
+                        <Input
+                          type="text"
+                          variant="flushed"
+                          placeholder="FirstName"
+                          fontSize={"20px"}
+                          isInvalid
+                          errorBorderColor="#AA8B56"
+                          onChange={(event) => {
+                            setFirstname(event.target.value);
+                          }}
+                          value={firstName}
+                        />
+                      </FormControl>
+                    </Box>
+                    <Box>
+                      <FormControl id="lastName">
+                        {/* <FormLabel fontSize={'20px'} >Last Name</FormLabel> */}
+                        <Input
+                          type="text"
+                          variant="flushed"
+                          placeholder="Lastname"
+                          fontSize={"20px"}
+                          isInvalid
+                          errorBorderColor="#AA8B56"
+                          onChange={(event) => {
+                            setLastname(event.target.value);
+                          }}
+                          value={lastName}
+                        />
+                      </FormControl>
+                    </Box>
 
-            <FormControl id="email" isRequired>
-              {/* <FormLabel>Email address</FormLabel> */}
-              <Input type="email"  variant='flushed' placeholder='Email' fontSize={'20px'} isInvalid
-    errorBorderColor='#AA8B56'/>
-            </FormControl>
+                    <FormControl id="email" isRequired>
+                      {/* <FormLabel>Email address</FormLabel> */}
+                      <Input
+                        type="email"
+                        variant="flushed"
+                        placeholder="Email"
+                        fontSize={"20px"}
+                        isInvalid
+                        errorBorderColor="#AA8B56"
+                        onChange={(event) => {
+                          setEmail(event.target.value);
+                        }}
+                        value={email}
+                      />
+                    </FormControl>
 
-            <Stack spacing={10} pt={2}>
-              <Button
-                loadingText="Submitting"
-                size="md"
-                bg={'#CFB9AC'}
-                color={'white'}
-                _hover={{
-                  bg: '#CFB9AC',
-                }} fontSize={'20px'}>
-                SUBMIT
-              </Button>
-            </Stack>
-            <Stack pt={6}>
-              <Text align={'center'} fontSize={'20px'}>
-                Already a user? <Link color={'#AC4425'} fontSize={'20px'}>Login</Link>
-              </Text>
-            </Stack>
-          </Stack>
-        </Box>
-      </Stack>
-    </Flex>
-  </Box>
-  </Flex>
-    </Box>
+                    <Stack spacing={10} pt={2}>
+                      <Button
+                        loadingText="Submitting"
+                        size="md"
+                        bg={"#CFB9AC"}
+                        color={"white"}
+                        _hover={{
+                          bg: "#CFB9AC",
+                        }}
+                        fontSize={"20px"}
+                        type="submit"
+                        // onSubmit={(event)=>{
+                        //  handleSubmit(event)
+                        // }}
+                      >
+                        SUBMIT
+                      </Button>
+                    </Stack>
+                    <Stack pt={6}>
+                      <Text align={"center"} fontSize={"20px"}>
+                        Already a user?{" "}
+                        <Link color={"#AC4425"} fontSize={"20px"}>
+                          Login
+                        </Link>
+                      </Text>
+                    </Stack>
+                  </Stack>
+                </Box>
+              </Stack>
+            </Flex>
+          </Box>
+        </Flex>
+      </Box>
+    </form>
   );
 }
-
-
-
-
-
