30be7a8aa0bc77426ee43a44ddf563317fd1dfba
Pimporn <pimporn.kate@gmail.com>
2022-11-14 13:22:15 +0700

    add navbarlandpage

--- watch.session.sql
+++ watch.session.sql
@@ -1,1 +1,19 @@
-SELECT * FROM users
+SELECT * FROM usersINSERT INTO products (
+    product_id,
+    product_name,
+    image_brand,
+    image,
+    description,
+    price
+    created_at,
+    updated_at
+  )
+VALUES (
+    product_id:integer,
+    'product_name:character varying',
+    'image_brand:text',
+    'image:character varying',
+    'description:text',
+    'created_at:timestamp with time zone',
+    'updated_at:timestamp with time zone'
+  );
