895a2641d832293f0ae5606dd109be2d19114e87
Napassorn B <nanaps.napassorn@gmail.com>
2022-11-14 23:06:28 +0700

    finish sprin 1

--- client/src/pages/loginPage.js
+++ client/src/pages/loginPage.js
@@ -1,18 +1,13 @@
-import { Box, Image } from "@chakra-ui/react";
+import { Box} from "@chakra-ui/react";
 import { LoginForm } from "../components/loginForm";
+import { Time } from "../components/time";
 
 export function LoginPage() {
   return (
-    <Box w="100vw" bg="#F2F2F2">
+    <Box w="100vw" bg="#FFFFFF">
       <Box position="relative" zIndex={2}>
+        <Time />
         <LoginForm />
-        <Box position="absolute" top="0px" left="340px" zIndex={-1}>
-          <Image
-            src="https://i.kinja-img.com/gawker-media/image/upload/s--xyDzs1x2--/c_fit,fl_progressive,q_80,w_636/varrsizeys31wesl2eic.gif"
-            alt="clock"
-            boxSize="200px"
-          />
-        </Box>
       </Box>
     </Box>
   );
