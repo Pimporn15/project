15b2fd501d929f9ddd6fdb484b5a5674053d2f7f
Pimporn <pimporn.kate@gmail.com>
2022-11-15 17:13:03 +0700

    rebase

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,24 @@
 import React from "react";
 import { ChakraProvider } from "@chakra-ui/react";
 
+<<<<<<< HEAD
 import {Register} from "./pages/Registerpage";
+=======
+import { Register } from "./pages/Registerpage";
+>>>>>>> 8e92e5a (updatestate)
 
 function App() {
   return (
     <ChakraProvider>
+<<<<<<< HEAD
       <ProductListingPage />
       {/* <CartsPage/> */}
       {/* <Landingpage /> */}
       <Navbar />
       <Landingpage /> <Register />
+=======
+      <Register />
+>>>>>>> 8e92e5a (updatestate)
     </ChakraProvider>
   );
 }
