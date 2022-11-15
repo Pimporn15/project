05067e8d32096c1564d33bedf7599edd76f2be08
Pimporn <pimporn.kate@gmail.com>
2022-11-15 22:33:01 +0700

    fix:fix debug

--- server/utils/db.js
+++ server/utils/db.js
@@ -1,7 +1,7 @@
 import * as pg from "pg";
-import dotenv from "dotenv";
+
 const { Pool } = pg.default;
-dotenv.config();
+
 const pool = new Pool({
   connectionString: `	postgres://tuyaktvz:${process.env.PASSWORD}@tiny.db.elephantsql.com/tuyaktvz`,
 });
