433a87911d6480a6842a36513495b89a709bbe65
Pimporn <pimporn.kate@gmail.com>
2022-11-15 22:36:18 +0700

    fix: process .env

--- server/utils/db.js
+++ server/utils/db.js
@@ -1,9 +1,9 @@
 import * as pg from "pg";
-
+import dotenv from "dotenv";
 const { Pool } = pg.default;
-
+dotenv.config();
 const pool = new Pool({
-  connectionString:postgres://tuyaktvz:${process.env.PASSWORD}@tiny.db.elephantsql.com/tuyaktvz
+  connectionString: `	postgres://tuyaktvz:${process.env.PASSWORD}@tiny.db.elephantsql.com/tuyaktvz`,
 });
 
 export { pool };
