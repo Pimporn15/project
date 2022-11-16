9215b9fa3197a774c3ebf29cc28cb759f7b6b6e1
Pimporn <pimporn.kate@gmail.com>
2022-11-16 10:30:55 +0700

    watch

--- watch.session.sql
+++ watch.session.sql
@@ -1,1 +1,3 @@
-SELECT * FROM products
+UPDATE products
+SET image='https://images.unsplash.com/photo-1596516109370-29001ec8ec36?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
+WHERE product_id = 22
