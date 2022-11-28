8d059dc9cbd37110a25e5e1bc3e6382f7cdd0385
Napassorn B <nanaps.napassorn@gmail.com>
2022-11-28 10:10:25 +0700

    add:add calculate price function  for one item

--- client/src/pages/inCart.js
+++ client/src/pages/inCart.js
@@ -1,137 +1,221 @@
-import {
-  Box,
-  Button,
-  Text,
-  Image,
-  Flex,
-  Spacer,
- 
-} from "@chakra-ui/react";
+import { Box, Button, Text, Image, Flex, Spacer } from "@chakra-ui/react";
 import { useContext } from "react";
-import { CartContext} from "../App"; 
+import { CartContext } from "../App";
 import { useState } from "react";
 
-
-
-
-
-
 export function CART() {
+  const contextValue = useContext(CartContext); // mycart  อยู่ในนี้
+  const {
+    mycart,
+    setmyCart,
+    cartItem,
+    setTotalPrice,
+    totalPrice,
+    totalQuantity,
+    quantity,
+    handleAddCounter,
+    handleMinusCounter,
+    addProductToCart,
+  } = useContext(CartContext);
 
-  let contextValue = useContext(CartContext)    // mycart  อยู่ในนี้
-  let [me, setMe] = useState([])
-
-  // let removeItem = () => {
-  //   localStorage.removeItem("mycart.value[0]")
+  // const [counter1, setCounter1] = useState(1);
+  // function handleAddCounter1() {
+  //   if (counter1 >= 10) {
+  //     return;
+  //   }
+  //   setCounter1(counter1 + 1);
   // }
 
- 
-  const [counter1, setCounter1] = useState(1)
-    function handleAddCounter1() {
-      if(counter1 >= 10){
-        return
-        }setCounter1(counter1 + 1);
-    }
+  // function handleMinusCounter1() {
+  //   if (counter1 === 1) {
+  //     return;
+  //   }
+  //   setCounter1(counter1 - 1);
+  // }
 
-    function handleMinusCounter1() {
-      if(counter1 === 1){
-        return               
-       } setCounter1(counter1 - 1)
-    }
+  // const [counter2, setCounter2] = useState(1);
+  // function handleAddCounter2() {
+  //   if (counter2 >= 10) {
+  //     return;
+  //   }
+  //   setCounter2(counter2 + 1);
+  // }
 
-    const [counter2, setCounter2] = useState(1)
-    function handleAddCounter2() {
-      if(counter2 >= 10){
-        return
-      }setCounter2(counter2 + 1);
-    }
+  // function handleMinusCounter2() {
+  //   if (counter2 === 1) {
+  //     return;
+  //   }
+  //   setCounter2(counter2 - 1);
+  // }
 
-    function handleMinusCounter2() {     
-      if(counter2 === 1){
-        return                         
-      }setCounter2(counter2 - 1)
-    }
+  // const [counter3, setCounter3] = useState(1);
+  // function handleAddCounter3() {
+  //   if (counter3 >= 10) {
+  //     return;
+  //   }
+  //   setCounter3(counter3 + 1);
+  // }
 
-    const [counter3, setCounter3] = useState(1)
-    function handleAddCounter3() {
-      if(counter3 >= 10){
-        return
-      }setCounter3(counter3 + 1);
-    }
+  // function handleMinusCounter3() {
+  //   if (counter3 === 1) {
+  //     return;
+  //   }
+  //   setCounter3(counter3 - 1);
+  // }
 
-    function handleMinusCounter3() {         
-      if(counter3 === 1){
-        return                               
-       }setCounter3(counter3 - 1)
-    }
+  const getPrice = (value, quantity) => {
+    const total = value[0].price * quantity;
+    setTotalPrice(total);
+  };
 
-
-    let handleRemove = () => {
-      const sum = contextValue.mycart.filter((value) => value.id !== value[1].id)
-      contextValue.setmyCart(sum);
-
-    }
-
-  
-  
-  console.log(contextValue.mycart)
-
- 
-  
+  let handleRemove = () => {
+    const sum = contextValue.mycart.filter((value) => value.id !== value[1].id);
+    contextValue.setmyCart(sum);
+  };
 
   return (
-   
     // Navbar ==================================================================================
-   
-    <Box as = "Big-Box"> 
-    {/* {contextValue.mycart.map((value)=>{
+
+    <Box as="Big-Box">
+      {/* {contextValue.mycart.map((value)=>{
       return( */}
-        <Box position= "relative" as ="nav" bg="bgsurface" boxShadow = "lg" w="100%" >
+      <Box position="relative" as="nav" bg="bgsurface" boxShadow="lg" w="100%">
+        <Flex justify="flex-end">
+          <Button fontSize="15" variant="ghost" color="#AA8B56">
+            HOME
+          </Button>
+          <Button fontSize="15" variant="ghost" color="#AA8B56">
+            PRODUCTS
+          </Button>
+          <Button fontSize="15" variant="ghost" color="#AA8B56">
+            ACCESSORIES
+          </Button>
+          <Button fontSize="15" variant="ghost" color="#AA8B56">
+            LOG IN
+          </Button>
+          <Button
+            fontSize="15"
+            variant="ghost"
+            color="#585858"
+            onClick={() => {
+              contextValue.setmyCart(
+                contextValue.mycart.filter((value) => value.id !== value.id)
+              );
+            }}
+          >
+            CLEAR YOUR CART
+            <Image
+              mt="-1"
+              boxSize="30px"
+              src="picture/Bin.png"
+              className="bin"
+              alt="bin"
+            />
+          </Button>
 
-    <Flex justify = "flex-end">
-
-        <Button  fontSize="15" variant="ghost" color="#AA8B56" >HOME</Button>
-        <Button  fontSize="15" variant="ghost" color="#AA8B56">PRODUCTS</Button>
-        <Button  fontSize="15" variant="ghost" color="#AA8B56">ACCESSORIES</Button>
-        <Button  fontSize="15" variant="ghost" color="#AA8B56">LOG IN</Button>
-        <Button  fontSize="15" variant="ghost" color="#585858" onClick= {()=>{contextValue.setmyCart(contextValue.mycart.filter(value=>value.id !== value[0].id))}}  
-  >CLEAR YOUR CART <Image mt="-1" boxSize="30px" src="picture/Bin.png" className="bin" alt="bin"/>
-</Button>
-
-        {/* <Button  fontSize="15" variant="ghost" color="#AA8B56">   <Image boxSize="30px" src="https://img.icons8.com/dotty/512/favorite-cart.png" className="pic2" alt=""/>    </Button> */}
-    </Flex>
-</Box>
+          {/* <Button  fontSize="15" variant="ghost" color="#AA8B56">   <Image boxSize="30px" src="https://img.icons8.com/dotty/512/favorite-cart.png" className="pic2" alt=""/>    </Button> */}
+        </Flex>
+      </Box>
       {/* )
     })} */}
 
-<Box position="relative" mt="20" ml={[10,30,250]}> <Text fontSize="40px"> CART</Text> <Text fontSize={[20,25,30]}>Product Name</Text> </Box>
-<Box fontSize={[20,25,30]} position="absolute" mt={[-7,-10]} ml={[200,358,1000]} > Price </Box>
-<Box fontSize ={[20,25,30]} position="absolute" ml={[300,445,1195]}mt={[-7,-10]} >Quantity</Box>
-<Box fontSize ={[20,25,30]} position="absolute" ml={[400,550,1400]} mt={[-7,-10]} >Total</Box>
+      <Box position="relative" mt="20" ml={[10, 30, 250]}>
+        <Text fontSize="40px"> CART</Text>
+        <Text fontSize={[20, 25, 30]}>Product Name</Text>
+      </Box>
+      <Box
+        fontSize={[20, 25, 30]}
+        position="absolute"
+        mt={[-7, -10]}
+        ml={[200, 358, 1000]}
+      >
+        Price
+      </Box>
+      <Box
+        fontSize={[20, 25, 30]}
+        position="absolute"
+        ml={[300, 445, 1195]}
+        mt={[-7, -10]}
+      >
+        Quantity
+      </Box>
+      <Box
+        fontSize={[20, 25, 30]}
+        position="absolute"
+        ml={[400, 550, 1400]}
+        mt={[-7, -10]}
+      >
+        Total
+      </Box>
 
-{contextValue.mycart.map((value)=>{
-  console.log(value[0])
-  
-return(
-  
-// กล่อง บรรจุ สินค้า ที่ add มา ========================================================================================
-<Box>
-  <Box position="relative" h="40" border="1px" borderTopColor="white" borderRightColor="white" borderLeftColor="white"  borderBottomColor="#DEDEDE" > </Box>
-<Box position="absolute" boxShadow ="2xl" h ={[50,100,120]} w ={[50,100,200]} mt ={[-130]} ml ={[5]} ><Image boxSize="full" src={value[0].image} className="pic1" alt=""/>  </Box>
-<Box position="absolute" ml={[80,125,280]}  mt={[-90]} as="b" > {value[0].product_name}</Box>
+      {contextValue.mycart.map((value) => {
+        return (
+          // กล่อง บรรจุ สินค้า ที่ add มา ========================================================================================
+          <Box>
+            <Box
+              position="relative"
+              h="40"
+              border="1px"
+              borderTopColor="white"
+              borderRightColor="white"
+              borderLeftColor="white"
+              borderBottomColor="#DEDEDE"
+            ></Box>
+            <Box
+              position="absolute"
+              boxShadow="2xl"
+              h={[50, 100, 120]}
+              w={[50, 100, 200]}
+              mt={[-130]}
+              ml={[5]}
+            >
+              <Image
+                boxSize="full"
+                src={value[0].image}
+                className="pic1"
+                alt=""
+              />
+            </Box>
+            <Box position="absolute" ml={[80, 125, 280]} mt={[-90]} as="b">
+              {value[0].product_name}
+            </Box>
 
+            {/* <Button flex position="absolute" size="xs"  bg="#585858" ml="220" mt="-140" color="tomato" onClick={()=> handleRemove(value.id)}> <Text  fontSize="20"> x </Text> </Button>  */}
 
- {/* <Button flex position="absolute" size="xs"  bg="#585858" ml="220" mt="-140" color="tomato" onClick={()=> handleRemove(value.id)}> <Text  fontSize="20"> x </Text> </Button>  */}
+            <Box position="absolute" ml={[50, 360, 1015]} mt={[-90]} as="b">
+              {value[0].price}
+            </Box>
+            <Box position="absolute" ml={[50, 450, 1215]} mt={[-90]} as="b">
+              <Button
+                onClick={() => {
+                  handleMinusCounter(value[0].product_id);
+                }}
+                bg="#DEDEDE"
+                size="xs"
+              >
+                -
+              </Button>
+              <Text as="b" fontSize="18px">
+                {quantity}
+              </Text>
+              <Button
+                onClick={() => {
+                  handleAddCounter(value[0].product_id);
+                }}
+                bg="#DEDEDE"
+                size="xs"
+              >
+                +
+              </Button>
+            </Box>
+            <Box position="absolute" ml={[50, 560, 1415]} mt={[-90]} as="b">
+              {value[0].price * quantity}
+            </Box>
+          </Box>
+        );
+      })}
 
-<Box position="absolute" ml={[50,360,1015]} mt={[-90]} as="b" >{value[0].price}</Box>
-<Box position="absolute" ml={[50,450,1215]} mt={[-90]} as="b" > <Button onClick={handleMinusCounter1} bg="#DEDEDE" size="xs"> - </Button>  <Text as="b" fontSize="18px" >  {counter1} </Text>  <Button onClick={handleAddCounter1} bg="#DEDEDE" size="xs"> + </Button>      
- </Box>
-<Box position="absolute" ml={[50,560,1415]} mt={[-90]} as="b" > 2222</Box>
-</Box>
-)
-})}
-
-{/* {contextValue.mycart.map((value)=>{
+      {/* {contextValue.mycart.map((value)=>{
 return(
   <Box>
 <Box position="relative" h="40" border="1px" borderTopColor="white" borderRightColor="white" borderLeftColor="white"  borderBottomColor="#DEDEDE" > </Box>
@@ -144,25 +228,16 @@ return(
 )
 })} */}
 
-
-
-
-{/* <Box position="relative" h="40" border="1px" borderTopColor="white" borderRightColor="white" borderLeftColor="white"  borderBottomColor="#DEDEDE" > </Box>
+      {/* <Box position="relative" h="40" border="1px" borderTopColor="white" borderRightColor="white" borderLeftColor="white"  borderBottomColor="#DEDEDE" > </Box>
 <Box position="absolute" boxShadow ="2xl" h ={[50,100,120]} w ={[50,100,200]} mt ={[-130]} ml ={[5]} ><Image boxSize="full" src={value.image} className="pic1" alt=""/>  </Box>
 <Box position="absolute" ml={[80,125,280]}  mt={[-90]} as="b" > {value.product_name}</Box>
 <Box position="absolute" ml={[50,360,1015]} mt={[-90]} as="b" >{value.price}</Box>
 <Box position="absolute" ml={[50,450,1215]} mt={[-90]} as="b" ><Button onClick={handleMinusCounter3} bg="#DEDEDE" size="xs"> - </Button>  <Text as="b" fontSize="18px" >  {counter3} </Text>  <Button onClick={handleAddCounter3} bg="#DEDEDE" size="xs"> + </Button> </Box>
 <Box position="absolute" ml={[50,560,1415]} mt={[-90]} as="b" > 2222</Box>  */}
 
-{/* </Box> */}
+      {/* </Box> */}
 
-
-
-
-
-
-
-{/* {contextValue.mycart.map((value,index)=>{
+      {/* {contextValue.mycart.map((value,index)=>{
             return(
                 <Box key={index} boxShadow ="2xl" h ={[100,300,500]} w ={[100,300,600]} mt ="175" ml ={[30,70,100]} > 
                 <Image boxSize="full" src={value.image} className="pic1" alt=""/>
@@ -171,27 +246,22 @@ return(
             )
          })}  */}
 
-          {/* Footer============================================================================================= */}
-
-          <Flex justifyContent="center" color= "#AA8B56" mr="100" ml={[50,100,300]} mt="300">
-          <Text fontSize={{base:'15px',sm:'20px', md:'20px', lg:'px'}}>Copyright©2022 Watchodile.All Right Reserved.</Text>
-          </Flex>
-
-
-</Box>
-
-
-
-      )
-
-
-
-
-
-
-
-  }
-
-  
-   
-  
+      {/* Footer============================================================================================= */}
+      <Flex direction="column" width="35%" align="start" ml="80%" mt="5%">
+        <Text fontSize="1.5rem">Total:</Text>
+        <Button>Check Out</Button>
+      </Flex>
+      <Flex
+        justifyContent="center"
+        color="#AA8B56"
+        mr="100"
+        ml={[50, 100, 300]}
+        mt="300"
+      >
+        <Text fontSize={{ base: "15px", sm: "20px", md: "20px", lg: "px" }}>
+          Copyright©2022 Watchodile.All Right Reserved.
+        </Text>
+      </Flex>
+    </Box>
+  );
+}
--- client/src/pages/products-detail1.js
+++ client/src/pages/products-detail1.js
@@ -1,202 +1,223 @@
-import {
-    Box,
-    Button,
-    Text,
-    Image,
-    Flex,
-    Stack,
-   
-  } from "@chakra-ui/react";
-  
-  import{useEffect, useState,useContext} from "react";
+import { Box, Button, Text, Image, Flex, Stack } from "@chakra-ui/react";
 
-  import axios from 'axios'
-  import React from "react";
-  
-  import { StarIcon } from "@chakra-ui/icons";
-  import { useNavigate, useParams } from "react-router-dom";
-  import { CartContext } from "../App";
-  
-  
+import { useEffect, useState, useContext } from "react";
 
+import axios from "axios";
+import React from "react";
 
+import { StarIcon } from "@chakra-ui/icons";
+import { useNavigate, useParams } from "react-router-dom";
+import { CartContext } from "../App";
 
+export function ProductDetail1() {
+  const [datas, setDatas] = useState([]);
+  const contextValue = useContext(CartContext);
+  const { handleAddCounter, handleMinusCounter, quantity } =
+    useContext(CartContext);
+  const Navigate = useNavigate();
 
-  
-  export function ProductDetail1() {
-    let [datas, setDatas] = useState([]);
+  const { id } = useParams();
 
-    let contextValue = useContext(CartContext);
-    
-    const Navigate = useNavigate();
+  useEffect(() => {
+    getPosts();
+  }, []);
 
-    
+  let getPosts = async () => {
+      let response = await axios.get(`http://localhost:4000/products/${id} `);
+      console.log(response)
+      setDatas(response.data.data); 
+  };
 
-    const {id} = useParams();
+  // const [counter, setCounter] = useState(1);
 
-    useEffect(()=>{
-        getPosts()
-    }, []);
+  // function handleAddCounter() {
+  //   if (counter >= 10) {
+  //     return;
+  //   }
+  //   setCounter(counter + 1);
+  // }
 
-   
-    let getPosts = async () => {
-    try{ 
-     
-       let response = await axios.get (`http://localhost:4000/products/${id} `)
-       setDatas(response.data.data)
-     
-        } catch (err){
-            console.log(err)
+  // function handleMinusCounter() {
+  //   if (counter === 1) {
+  //     return;
+  //   }
+  //   setCounter(counter - 1);
+  // }
 
-        }
-    }
-
-   
-
-    const [counter, setCounter] = useState(1)
-   
-
-    function handleAddCounter() {
-      if(counter >= 10){
-        return
-      }setCounter(counter + 1);
-    }
-
-
-
-
-    function handleMinusCounter() {              
-      if(counter === 1){
-        return
-      }setCounter(counter - 1)
-      
-      
-       
-    }
-    
-
-
- 
-    console.log(datas)
-    return (
-       
-        // Navbar ==================================================================================
-        <Box as = "Big-Box"> 
-            <Box as ="nav" bg="bgsurface" boxShadow = "lg" w="100%" >
-        <Flex justify = "flex-end">
-            <Button  fontSize="15" variant="ghost" color="#AA8B56" >HOME</Button>
-            <Button  fontSize="15" variant="ghost" color="#AA8B56">PRODUCTS</Button>
-            <Button  fontSize="15" variant="ghost" color="#AA8B56">ACCESSORIES</Button>
-            <Button  fontSize="15" variant="ghost" color="#AA8B56">LOG IN</Button>
-            <Button  fontSize="15" variant="ghost" color="#AA8B56">   <Image boxSize="30px" src="https://img.icons8.com/dotty/512/favorite-cart.png" className="pic2" alt=""/>    </Button>
+  console.log(datas);
+  return (
+    // Navbar ==================================================================================
+    <Box as="Big-Box">
+      <Box as="nav" bg="bgsurface" boxShadow="lg" w="100%">
+        <Flex justify="flex-end">
+          <Button fontSize="15" variant="ghost" color="#AA8B56">
+            HOME
+          </Button>
+          <Button fontSize="15" variant="ghost" color="#AA8B56">
+            PRODUCTS
+          </Button>
+          <Button fontSize="15" variant="ghost" color="#AA8B56">
+            ACCESSORIES
+          </Button>
+          <Button fontSize="15" variant="ghost" color="#AA8B56">
+            LOG IN
+          </Button>
+          <Button fontSize="15" variant="ghost" color="#AA8B56">
+            <Image
+              boxSize="30px"
+              src="https://img.icons8.com/dotty/512/favorite-cart.png"
+              className="pic2"
+              alt=""
+            />
+          </Button>
         </Flex>
-    </Box>
-        
-        {/* รูปใหญ่============================================================================================= */}
-         {datas.map((value,index)=>{
-            return(
-                <Box key={index} boxShadow ="2xl" h ={[100,300,500]} w ={[100,300,600]} mt ="175" ml ={[30,70,100]} > 
-                <Image boxSize="full" src={value.image} className="pic1" alt=""/>
-                 </Box> 
-            
-            )
-         })} 
-      
-          {/* ข้อมูลต่างๆของสินค้า================================================================================== */}
-          {datas.map((value, index)=>{
-            return(
-                <Flex key={index} bg="white-smoke" w={[100,200,500]} h="500" ml = {[100,400,900]} mt={[-100,-300,-500]} position="relative">
-           <Stack spacing={2}>
-            
-            <Text fontSize={{base:'10px',sm:'20px', md:'20px', lg:'30px'}}> {value.product_name} </Text>
+      </Box>
 
-
-            <Text fontSize={{base:'10px',sm:'10px', md:'15px', lg:'25px'}}>
-                <Flex> 
-            
-                <Box as="span" color="gray.600" fontSize="xl">
-                  <Box display="flex" mt="2" alignItems="center">
-                    {Array(5)
-                      .fill("")
-                      .map((_, i) => (
-                        <StarIcon
-                          key={i}
-                          color={i < value.rating ? "#fdd835" : "gray.300"}
-                        />
-                      ))}
+      {/* รูปใหญ่============================================================================================= */}
+      {datas.map((value, index) => {
+        return (
+          <Box
+            key={index}
+            boxShadow="2xl"
+            h={[100, 300, 500]}
+            w={[100, 300, 600]}
+            mt="175"
+            ml={[30, 70, 100]}
+          >
+            <Image boxSize="full" src={value.image} className="pic1" alt="" />
+          </Box>
+        );
+      })}
+      {/* ข้อมูลต่างๆของสินค้า================================================================================== */}
+      {datas.map((value, index) => {
+        console.log(value);
+        return (
+          <Flex
+            bg="white-smoke"
+            w={[100, 200, 500]}
+            h="500"
+            ml={[100, 400, 900]}
+            mt={[-100, -300, -500]}
+            position="relative"
+          >
+            <Stack spacing={2}>
+              <Text
+                fontSize={{ base: "10px", sm: "20px", md: "20px", lg: "30px" }}
+              >
+                {value.product_name}
+              </Text>
+              <Text
+                fontSize={{ base: "10px", sm: "10px", md: "15px", lg: "25px" }}
+              >
+                <Flex>
+                  <Box as="span" color="gray.600" fontSize="xl">
+                    <Box display="flex" mt="2" alignItems="center">
+                      {Array(5)
+                        .fill("")
+                        .map((_, i) => (
+                          <StarIcon
+                            key={i}
+                            color={i < value.rating ? "#fdd835" : "gray.300"}
+                          />
+                        ))}
+                    </Box>
                   </Box>
-                </Box>
-          
 
-            <Flex position="absolute" ml="150" mt="2.5">  <Button  borderColor="white" size="xs" variant="outline"> <Text fontSize={[5,10,12]}>{value.rating} Points and Reviews.. </Text> </Button>  </Flex> 
-                 </Flex>
-             </Text>
+                  <Flex position="absolute" ml="150" mt="2.5">
+                    <Button borderColor="white" size="xs" variant="outline">
+                      <Text fontSize={[5, 10, 12]}>
+                        {value.rating} Points and Reviews..
+                      </Text>
+                    </Button>
+                  </Flex>
+                </Flex>
+              </Text>
 
+              <Text fontSize={{ base: "10px", md: "15px", lg: "20px" }}>
+                {value.description}
+              </Text>
 
-            <Text fontSize={{base:'10px', md:'15px', lg:'20px'}}> {value.description}</Text>
+              <Text fontSize="17"> Price</Text>
+              <Box pb="20px" as="b" color="tomato" fontSize="20">
+                ฿ {value.price}
+              </Box>
 
-            <Text fontSize="17"> Price</Text>
-            <Box pb="20px" as ="b" color="tomato" fontSize="20"> ฿ {value.price} </Box>
-         
-            {/* จำนวนสินค้า========================================================================================================================= */}
-              <Box m="100"> <Text fontSize="17px"> Quantity <Flex position="absolute" bg="white" display="flex" w="100px" h="30px" ml="70px" mt="-25px" justify="space-between"> <Button onClick={handleMinusCounter} bg="#CFB9AC" size="xs"> - </Button>  <Text as="b" fontSize="18px" >  {counter} </Text>  <Button onClick={handleAddCounter} bg="#CFB9AC" size="xs"> + </Button>      </Flex> </Text></Box>
+              {/* จำนวนสินค้า========================================================================================================================= */}
+              <Box m="100">
+                <Text fontSize="17px">
+                  Quantity
+                  <Flex
+                    position="absolute"
+                    bg="white"
+                    display="flex"
+                    w="100px"
+                    h="30px"
+                    ml="70px"
+                    mt="-25px"
+                    justify="space-between"
+                  >
+                    <Button onClick={handleMinusCounter} bg="#CFB9AC" size="xs">
+                      -
+                    </Button>
+                    <Text as="b" fontSize="18px">
+                      {quantity}
+                    </Text>
+                    <Button onClick={handleAddCounter} bg="#CFB9AC" size="xs">
+                      +
+                    </Button>
+                  </Flex>
+                </Text>
+              </Box>
             </Stack>
           </Flex>
-            )
-          })}
+        );
+      })}
+      {/* ปุ่มกด add to cart ==================================================================================== */}
+      {/* {datas.map((value, index)=>{ */}
+      {/* console.log(contextValue.mycart) */}
+      {/* return( */}
 
-          
-
-
-         {/* ปุ่มกด add to cart ==================================================================================== */}
-       
-          
-            {datas.map((value, index)=>{
-            // console.log(contextValue.mycart)
-               return(
-                
-
-                <Box ml = {[100,400,900]} >
-                  
-                <Button onClick={()=>{
-                  console.log(contextValue.mycart)
-                  contextValue.setmyCart([...contextValue.mycart,datas ])
-                  // setmyCart([...datas,datas] )
-                  Navigate("/cart")
-                }} size="lg" bg="#CFB9AC" color="white">
-                <Box > <Image boxSize="35px" src="https://img.icons8.com/nolan/512/shopping-cart.png" className="pic2" alt=""/> </Box>    <Box mr="15px">ADD TO CART</Box>
-                </Button>
-               
-                </Box>
-                )
+      <Box ml={[100, 400, 900]}>
+        <Button
+          onClick={() => {
+            contextValue.setmyCart([...contextValue.mycart, datas]);
+            Navigate("/cart");
+            console.log(contextValue.mycart);
+          }}
+          // onClick={(product, numOfQuantity) => {
+          //   addProductToCart();
+          // }}
+          size="lg"
+          bg="#CFB9AC"
+          color="white"
+        >
+          <Box>
+            <Image
+              boxSize="35px"
+              src="https://img.icons8.com/nolan/512/shopping-cart.png"
+              className="pic2"
+              alt=""
+            />
+          </Box>
+          <Box mr="15px">ADD TO CART</Box>
+        </Button>
+      </Box>
+      {/* )
               
 
-            })}
-         
-          
-          
-
-
-
-           {/* Footer============================================================================================= */}
-
-          <Flex justifyContent="center" color= "#AA8B56" mr="100" ml={[50,100,300]} mt="300">
-          <Text fontSize={{base:'15px',sm:'20px', md:'20px', lg:'px'}}>Copyright©2022 Watchodile.All Right Reserved.</Text>
-          </Flex>
-
-
-          </Box>
-          
-        
-
-
-
-    )
-   
+            })} */}
+      {/* Footer============================================================================================= */}
+      <Flex
+        justifyContent="center"
+        color="#AA8B56"
+        mr="100"
+        ml={[50, 100, 300]}
+        mt="300"
+      >
+        <Text fontSize={{ base: "15px", sm: "20px", md: "20px", lg: "px" }}>
+          Copyright©2022 Watchodile.All Right Reserved.
+        </Text>
+      </Flex>
+    </Box>
+  );
 }
-
-
-
-
-
-   
